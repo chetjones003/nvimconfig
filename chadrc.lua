14529a07bc3cd98be4fd81385aba930600d4365e
Chet Jones <chetjones003@gmail.com>
2023-06-14 19:21:58 -0500

    catppuccin color overrides

--- chadrc.lua
+++ chadrc.lua
@@ -1,6 +1,15 @@
 ---@type ChadrcConfig 
 local M = {}
-M.ui = {theme = 'doomchad'}
+M.ui = {
+  theme = 'catppuccin',
+  hl_override = {
+    Comment = { fg = "#777777" },
+    Visual = { bg = "#1c1c1c" },
+    NvimTreeNormal = { bg = "NONE" },
+    NvimTreeNormalNC = { bg = "NONE" },
+    ColorColumn = { bg = "#1c1c1c" },
+  }
+}
 M.plugins = "custom.plugins"
 M.mappings = require "custom.mappings"
 return M

a4cf0b938fefc729a2fc94430ff2f2e755ea853d
Chet Jones <chetjones003@gmail.com>
2023-10-07 23:58:06 -0500

    feat: some move commands

--- lua/chetj/remap.lua
+++ lua/chetj/remap.lua
@@ -11,3 +11,9 @@ vim.keymap.set("i", "<C-c>", "<Esc>")
 vim.keymap.set("n", "Q", "<nop>")
 
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
+vim.keymap.set("v", "J", ":m '>+1<cr>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<cr>gv=gv")
+
+vim.keymap.set("v", "<", "<gv")
+vim.keymap.set("v", ">", ">gv")

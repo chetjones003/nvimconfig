623a31e81be0f16b332fe06ec33fcb581ebc8e5c
Chet Jones <chetjones003@gmail.com>
2023-10-07 21:58:46 -0500

    fix: <c-j> <c-k> remaps interfere with tmux-navigator

--- lua/chetj/remap.lua
+++ lua/chetj/remap.lua
@@ -14,8 +14,6 @@ vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
-vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
-vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
 vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 

4c2132b3de50db8cfce415d30185eba2c141e15a
Chet Jones <chetjones003@gmail.com>
2023-10-16 22:10:17 -0500

    fix: add pcall for safety

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,26 +1,55 @@
-local lsp_zero = require("lsp-zero")
+local lsp_ok, lsp_zero = pcall(require, "lsp-zero")
+if not lsp_ok then
+    return
+else
+    lsp_zero.setup_servers({"tsserver", "pyright", "emmet_language_server", "cssls"})
+end
 
-require("lspconfig").lua_ls.setup({
-    settings = {
-        Lua = {
-            diagnostics = {
-                globals = {"vim"},
+local mason_ok, mason = pcall(require, "mason")
+if not mason_ok then
+    return
+else
+    mason.setup({
+        PATH = "prepend",
+    })
+end
+
+local masonconfig_ok, mason_lspconfig = pcall(require, "mason-lspconfig")
+if not masonconfig_ok then
+    return
+else
+    mason_lspconfig.setup({
+        ensure_installed = {
+            "rust_analyzer",
+            "tsserver",
+            "pyright",
+            "emmet_language_server",
+            "cssls",
+            "eslint",
+            "lua_ls"
+        },
+        handlers = {
+            lsp_zero.default_setup,
+        },
+    })
+end
+
+
+local lspconfig_ok, lspconfig = pcall(require, "lspconfig")
+if not lspconfig_ok then
+    return
+else
+    lspconfig.lua_ls.setup({ settings = { Lua = {
+                diagnostics = {
+                    globals = { "vim" },
+                },
             },
         },
-    },
-})
+    })
 
-require("lspconfig").rust_analyzer.setup({})
+    lspconfig.rust_analyzer.setup({})
+end
 
-require("mason").setup({})
-require("mason-lspconfig").setup({
-  ensure_installed = {
-      "lua_ls",
-      "rust_analyzer",
-  },
-  handlers = {
-    lsp_zero.default_setup,
-  },
-})
 
-lsp_zero.setup_servers({"lua_ls", "rust_analyzer"})
+
+
--- after/plugin/rust.lua
+++ after/plugin/rust.lua
@@ -1,43 +1,37 @@
-local lsp_status_ok, lsp_zero = pcall(require, "lsp_zero")
-if not lsp_status_ok then
-    return
-end
-
-local rust_status_ok, rust_tools = pcall(require, "rust-tools")
-if not rust_status_ok then
-    return
-end
-
-local crates_status_ok, crates = pcall(require, "crates")
-if not crates_status_ok then
-    return
-end
-
-local cmp_status_ok, cmp = pcall(require, "cmp")
-if not cmp_status_ok then
+local lsp_ok, lsp_zero = pcall(require, "lsp-zero")
+if not lsp_ok then
     return
 end
 
 lsp_zero.on_attach(function(client, bufnr)
-    lsp_zero.default_keymaps({ buffer = bufnr })
+  lsp_zero.default_keymaps({buffer = bufnr})
 end)
 
+local rust_ok, rust_tools = pcall(require, "rust-tools")
+if not rust_ok then
+    return
+end
+
 rust_tools.setup({
-    server = {
-        on_attach = function(client, bufnr)
-            vim.keymap.set("n", "<leader>ra", rust_tools.code_action_group.code_action_group, { buffer = bufnr })
-            vim.keymap.set("n", "<leader>rr", rust_tools.runnables.runnables, { buffer = bufnr })
-        end
+  server = {
+    on_attach = function(client, bufnr)
+      vim.keymap.set("n", "<leader>ra", rust_tools.code_action_group.code_action_group, {buffer = bufnr})
+      vim.keymap.set("n", "<leader>rr", rust_tools.runnables.runnables, {buffer = bufnr})
+    end
+  },
+  dap = {
+    adapter = {
+      type = "executable",
+      command = "lldb-vscode",
+      name = "rt_lldb",
     },
-    dap = {
-        adapter = {
-            type = "executable",
-            command = "lldb-vscode",
-            name = "rt_lldb",
-        },
-    },
+  },
 })
 
+local crates_ok, crates = pcall(require, "crates")
+if not crates_ok then
+    return
+end
 crates.setup({
     src = {
         cmp = {
@@ -45,12 +39,3 @@ crates.setup({
         },
     },
 })
-
-cmp.setup {
-    sources = {
-        { name = "path" },
-        { name = "buffer" },
-        { name = "nvim_lsp" },
-        { name = "crates" },
-    },
-}

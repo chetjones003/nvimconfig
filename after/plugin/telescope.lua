f63baa5df38906f78fabe186c7dd512666489603
Chet Jones <chetjones003@gmail.com>
2023-10-16 22:10:57 -0500

    fix: add pcall for safety

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -3,7 +3,12 @@ if not actions_setup then
     return
 end
 
-require("telescope").setup({
+local telescope_ok, telescope = pcall(require, "telescope")
+if not telescope_ok then
+    return
+end
+
+telescope.setup({
     defaults = {
         mappings = {
             i = {

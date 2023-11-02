fd919d9dcc70cf8b711cf65262306afa839974a3
Chet Jones <chetjones003@gmail.com>
2023-11-02 17:43:23 -0500

    feat: setup javascript debugging

--- after/plugin/dap.lua
+++ after/plugin/dap.lua
@@ -1,31 +1,53 @@
 local dap, dapui = require("dap"), require("dapui")
 dapui.setup({
-    layouts = {
+  layouts = {
+    {
+      elements = {
         {
-            elements = {
-                {
-                    id = "watches",
-                    size = 0.50,
-                    expanded = true,
-                },
-                {
-
-                    id = "scopes",
-                    size = 0.50
-                },
-            },
-            position = "left",
-            size = 40
+          id = "watches",
+          size = 0.50,
+          expanded = true,
         },
+        {
+
+          id = "scopes",
+          size = 0.50
+        },
+      },
+      position = "left",
+      size = 40
     },
+  },
 })
 
 dap.listeners.after.event_initialized["dapui_config"] = function()
-    dapui.open()
+  dapui.open()
 end
 dap.listeners.before.event_terminated["dapui_config"] = function()
-    dapui.close()
+  dapui.close()
 end
 dap.listeners.before.event_exited["dapui_config"] = function()
-    dapui.close()
+  dapui.close()
+end
+
+dap.adapters["pwa-node"] = {
+  type = "server",
+  host = "127.0.0.1",
+  port = 8123,
+  executable = {
+    command = "js-debug-adapter",
+  }
+}
+
+for _, language in ipairs { "typescript", "javascript" } do
+  dap.configurations[language] = {
+    {
+      type = "pwa-node",
+      request = "launch",
+      name = "Launch File",
+      program = "${file}",
+      cwd = "${workspaceFolder}",
+      runtimeExecutable = "node",
+    },
+  }
 end

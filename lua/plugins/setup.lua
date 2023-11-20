1dea8a2f22123c50748520ca19bf2ef977e7f67c
Chet Jones <chetjones003@gmail.com>
2023-11-19 22:31:38 -0600

    fix: change lualine theme to gruvbox

--- lua/plugins/setup.lua
+++ lua/plugins/setup.lua
@@ -69,7 +69,7 @@ require("lazy").setup({
     "nvim-lualine/lualine.nvim",
     requires = { "nvim-tree/nvim-web-devicons", opt = true },
     opts = {
-      theme = "tokyonight",
+      theme = "gruvbox",
     },
   },
 

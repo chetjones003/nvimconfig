64d844c9268a497844891f2fb15df161370a9595
Chet Jones <chetjones003@gmail.com>
2023-12-26 15:57:05 -0600

    feat: add keymaps for my most used features

--- lua/cjvim/config/remaps.lua
+++ lua/cjvim/config/remaps.lua
@@ -17,3 +17,9 @@ vim.keymap.set("v", ">", ">gv")
 
 vim.keymap.set("n", "<S-l>", vim.cmd.bnext)
 vim.keymap.set("n", "<S-h>", vim.cmd.bprevious)
+
+vim.keymap.set("n", "<leader>e", vim.cmd.NvimTreeToggle)
+vim.keymap.set("n", "<leader>fj", "<cmd>Telescope commands<cr>")
+vim.keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>")
+vim.keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>")
+vim.keymap.set("n", "<leader>fs", "<cmd>Telescope current_buffer_fuzzy_find<cr>")

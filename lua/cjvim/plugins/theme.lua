e843cb2f6509034abd8118a10d511e61c1c6a3e8
Chet Jones <chetjones003@gmail.com>
2023-12-26 18:26:13 -0600

    feat: rose-pine (like prime)

--- lua/cjvim/plugins/theme.lua
+++ lua/cjvim/plugins/theme.lua
@@ -1,49 +1,57 @@
-require("catppuccin").setup({
-    flavour = "mocha", -- latte, frappe, macchiato, mocha
-    background = { -- :h background
-        light = "latte",
-        dark = "mocha",
-    },
-    transparent_background = true, -- disables setting the background color.
-    show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
-    term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
-    dim_inactive = {
-        enabled = false, -- dims the background color of inactive window
-        shade = "dark",
-        percentage = 0.15, -- percentage of the shade to apply to the inactive window
-    },
-    no_italic = true, -- Force no italic
-    no_bold = true, -- Force no bold
-    no_underline = true, -- Force no underline
-    styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
-        comments = { "italic" }, -- Change the style of comments
-        conditionals = { "italic" },
-        loops = {},
-        functions = {},
-        keywords = {},
-        strings = {},
-        variables = {},
-        numbers = {},
-        booleans = {},
-        properties = {},
-        types = {},
-        operators = {},
-    },
-    color_overrides = {},
-    custom_highlights = {},
-    integrations = {
-        cmp = true,
-        gitsigns = true,
-        nvimtree = true,
-        treesitter = true,
-        notify = false,
-        mini = {
-            enabled = true,
-            indentscope_color = "",
-        },
-        -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
-    },
+require("rose-pine").setup({
+	--- @usage "auto"|"main"|"moon"|"dawn"
+	variant = "auto",
+	--- @usage "main"|"moon"|"dawn"
+	dark_variant = "main",
+	bold_vert_split = true,
+	dim_nc_background = false,
+	disable_background = true,
+	disable_float_background = false,
+	disable_italics = true,
+
+	--- @usage string hex value or named color from rosepinetheme.com/palette
+	groups = {
+		background = "base",
+		background_nc = "_experimental_nc",
+		panel = "surface",
+		panel_nc = "base",
+		border = "highlight_med",
+		comment = "muted",
+		link = "iris",
+		punctuation = "subtle",
+
+		error = "love",
+		hint = "iris",
+		info = "foam",
+		warn = "gold",
+
+		headings = {
+			h1 = "iris",
+			h2 = "foam",
+			h3 = "rose",
+			h4 = "gold",
+			h5 = "pine",
+			h6 = "foam",
+		}
+		-- or set all headings at once
+		-- headings = "subtle"
+	},
+
+	-- Change specific vim highlight groups
+	-- https://github.com/rose-pine/neovim/wiki/Recipes
+	highlight_groups = {
+		ColorColumn = { bg = "surface" },
+
+		-- Blend colours against the "base" background
+		CursorLine = { bg = "foam", blend = 10 },
+		StatusLine = { fg = "pine", bg = "love", blend = 10 },
+
+		-- By default each group adds to the existing config.
+		-- If you only want to set what is written in this config exactly,
+		-- you can set the inherit option:
+		IncSearch = { bg = "muted", fg = "base", inherit = false },
+	}
 })
 
--- setup must be called before loading
-vim.cmd.colorscheme "catppuccin"
+-- Set colorscheme after options
+vim.cmd("colorscheme rose-pine")

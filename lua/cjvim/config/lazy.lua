05b19c65b17899b9082791b9c6893fd9450fd247
Chet Jones <chetjones003@gmail.com>
2024-04-14 22:32:45 -0500

    fix: fix missing LuaSnip for cmp

--- lua/cjvim/config/lazy.lua
+++ lua/cjvim/config/lazy.lua
@@ -157,6 +157,7 @@ require("lazy").setup({
     "hrsh7th/nvim-cmp",
     event = "InsertEnter",
     dependencies = {
+      { "L3MON4D3/LuaSnip" },
       { "hrsh7th/cmp-path" },
       { "hrsh7th/cmp-nvim-lua" },
       { "hrsh7th/cmp-nvim-lsp" },
